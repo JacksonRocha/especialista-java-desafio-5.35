e5882afaee508001b52360f1901d14d2c8766827
jacksonrocha <jackson_lbr@hotmail.com>
2024-08-19 13:44:36 -0300

    Adicionando a classe principal e invocando os metodos

---
+++ src/main/java/jackson/rocha/Principal.java
@@ -0,0 +1,9 @@
+package jackson.rocha;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        CalcularFormasGeometricas.calcularAreaQuadrado(10);
+        CalcularFormasGeometricas.calcularAreaCirculo(15);
+    }
+}
